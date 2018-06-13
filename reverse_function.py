d0eb539c424c89644c350e98bd4622e7fed9c8e0
Kanishka <inboxkanishka@gmail.com>
2018-06-13 15:32:24 -0400

    Modified code to ask input from user

--- reverse_function.py
+++ reverse_function.py
@@ -1,7 +1,8 @@
+s = input("Type the string to be reversed:")
 def reverse(s):
     str = ""
     for i in s:
         str = i + str
     return str
-s = "1234567"
+#s = "1234567"
 print (reverse(s))
